getgrav/grav-plugin-form

commit ddf6e40cd8bc5f46a821837cdc52bd07af5f6c48

Potential fix for non-form _POST

form.php
@@ -181,7 +181,7 @@ class FormPlugin extends Plugin
             }
 
             // Handle posting if needed.
-            if (!empty($_POST)) {
+            if (!empty($_POST) && isset($_POST['data'])) {
 
                 $current_form_name = $this->getFormName($this->grav['page']);
                 $this->json_response = [];

commit 47718f9be8f711987e1595d9b0c15399c139b502

Fix regression with file uploads
Introduced by
https://github.com/getgrav/grav-plugin-form/commit/ddf6e40cd8bc5f46a8218
37cdc52bd07af5f6c48

The file upload does not rely on ‘data’, so it was failing

form.php
@@ -181,7 +181,7 @@ class FormPlugin extends Plugin
             }
 
             // Handle posting if needed.
-            if (!empty($_POST) && isset($_POST['data'])) {
+            if (!empty($_POST) && (isset($_POST['data']) || isset($_POST['__form-file-uploader__']))) {
 
                 $current_form_name = $this->getFormName($this->grav['page']);
                 $this->json_response = [];