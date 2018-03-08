513dc14fcaecff701341a4bb57475485c6153e32
Basile Trujillo [L0gIn] <basiletrujillo@gmail.com>
2018-03-08 16:57:28 +0100

    Remove Debugs

--- form.php
+++ form.php
@@ -84,8 +84,6 @@ class FormPlugin extends Plugin
      */
     public function onPageProcessed(Event $e)
     {
-        $this->grav['debugger']->addMessage('onPageProcessed');
-
         /** @var Page $page */
         $page = $e['page'];
         $page_route = $page->route();
@@ -128,7 +126,6 @@ class FormPlugin extends Plugin
      */
     public function onPagesInitialized()
     {
-        $this->grav['debugger']->addMessage('onPageSInitialized');
         $this->loadCachedForms();
     }
 
@@ -137,9 +134,6 @@ class FormPlugin extends Plugin
      */
     public function onPageInitialized()
     {
-        $this->grav['debugger']->addMessage('onPageInitialized');
-        $this->grav['debugger']->addMessage('$this->form: ');
-        $this->grav['debugger']->addMessage($this->form);
         $submitted = false;
         $this->json_response = [];
 
@@ -720,10 +714,8 @@ class FormPlugin extends Plugin
      */
     protected function loadCachedForms()
     {
-
-        $this->grav['debugger']->addMessage('loadCachedForms');
         // Get and set the cache of forms if it exists
-        /*list($forms, $flat_forms) = $this->grav['cache']->fetch($this->getFormCacheId());
+        list($forms, $flat_forms) = $this->grav['cache']->fetch($this->getFormCacheId());
 
         // Only store the forms if they are an array
         if (is_array($forms)) {
@@ -733,10 +725,7 @@ class FormPlugin extends Plugin
         // Only store the flat_forms if they are an array
         if (is_array($flat_forms)) {
             $this->flat_forms = array_merge($this->flat_forms, $flat_forms);
-        }*/
-
-        //$this->grav['debugger']->addMessage('Cached $forms:');
-        //$this->grav['debugger']->addMessage($forms);
+        }
     }
 
     /**
