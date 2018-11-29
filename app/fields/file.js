8d6514f76eb018da81115a69e21a0d3278a4f27c
Djamil Legato <djamil@djamil.it>
2018-11-29 12:47:51 -0800

    Pass all the data as entry for the file mock

--- app/fields/file.js
+++ app/fields/file.js
@@ -67,7 +67,8 @@ export default class FilesField {
                 status: Dropzone.ADDED,
                 accepted: true,
                 url: this.options.url,
-                removeUrl: data.remove
+                removeUrl: data.remove,
+                data
             };
 
             dropzone.files.push(mock);
