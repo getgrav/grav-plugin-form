7b5d8fef84fed2f5ffdd663887d3a938774d8f93
Djamil Legato <djamil@djamil.it>
2018-12-18 15:38:31 -0800

    Added support for tabs in form frontend

--- app/fields/index.js
+++ app/fields/index.js
@@ -1,5 +1,19 @@
+import $ from 'jquery';
 import FileInstances from './file';
 import ArrayInstances from './array';
 import PageMedia, { Instance as PageMediaInstances } from './media';
+
+$('body').on('touchstart click', '[data-tabid]', (event) => {
+    event && event.stopPropagation();
+    let target = $(event.currentTarget);
+
+    const panel = $(`[id="${target.data('tabid')}"]`);
+
+    target.siblings('[data-tabid]').removeClass('active');
+    target.addClass('active');
+
+    panel.siblings('[id]').removeClass('active');
+    panel.addClass('active');
+});
 
 export default { FileInstances, ArrayInstances, Media: { PageMedia, PageMediaInstances } };
