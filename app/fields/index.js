6474ab040ad9c168e964c334af828fdf8209f496
Djamil Legato <djamil@djamil.it>
2018-12-18 16:27:08 -0800

    Added support for toggleables/submit unchecked for frontend

--- app/fields/index.js
+++ app/fields/index.js
@@ -1,19 +1,7 @@
-import $ from 'jquery';
+import './form';
 import FileInstances from './file';
 import ArrayInstances from './array';
 import PageMedia, { Instance as PageMediaInstances } from './media';
-
-$('body').on('touchstart click', '[data-tabid]', (event) => {
-    event && event.stopPropagation();
-    let target = $(event.currentTarget);
-
-    const panel = $(`[id="${target.data('tabid')}"]`);
-
-    target.siblings('[data-tabid]').removeClass('active');
-    target.addClass('active');
-
-    panel.siblings('[id]').removeClass('active');
-    panel.addClass('active');
-});
+import './tabs';
 
 export default { FileInstances, ArrayInstances, Media: { PageMedia, PageMediaInstances } };
